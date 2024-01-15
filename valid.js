575d518284d1da18b0516f24bbed0e41bfd1a519
unknown <komalkhalkar2255@gmail.com>
2024-01-15 10:40:04 +0530

    login form

---
+++ valid.js
@@ -0,0 +1,25 @@
+function myfunction(){
+    var x= document.getElementById("pass");
+    
+    if(x.type === "password"){
+        x.type ="text";
+    }
+    else {
+        x.type ="password";
+
+    }
+}
+
+function validate(){
+    var password = document.getElementById("pass");
+    var length = document.getElementById("length");
+
+    if(password.value.length >= 8){
+        alert("Logged in Successfully");
+        
+        return false;
+    }
+    else {
+        alert("Login Failed");
+    }
+}
